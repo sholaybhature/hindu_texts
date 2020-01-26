237ce5d8c4bcb354b5ab5059215b5bf4005087be
sp1nalcord <55630723+sp1nalcord@users.noreply.github.com>
2020-01-26 15:03:22 +0530

    Add files via upload

---
+++ scripts/Ramayana/SundaraKanda/sundarakand_intro.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+import nltk
+import re
+import pandas as pd
+import xlrd
+
+def get_url():
+
+    excel_file = 'SundaraKanda.xlsx'
+    df = pd.read_excel(excel_file,header=2,usecols=['Verses'])
+    values = df.values.tolist()
+    number_of_verses = [item for sublist in values for item in sublist]
+
+    sarga_number = 1
+    number_of_chapters = 68
+    j = 1
+
+    text = ''
+
+    file = open("sundarakanda_english_intro.txt","w",encoding="utf-8")
+
+    while number_of_chapters >= sarga_number:
+        ls = []
+        url = requests.get(f"http://www.valmikiramayan.net/utf8/sundara/sarga{sarga_number}/sundarasans{sarga_number}.htm")
+        soup = BeautifulSoup(url.content, 'html.parser')
+
+        match = soup.findAll(class_='txt')
+        match = str(match)
+        #print(match)
+        tags = re.compile(r'<[^>]+>')
+        match = tags.sub('', match)
+        ls.append(match)
+
+        text = ' '.join(ls)
+        text = text.replace('[', '')
+        text = text.replace(']', '')
+        file.write('Chapter: ' + str(j))
+        file.write('\n')
+        file.write(text)
+        file.write('\n\n')
+
+        del ls
+
+        j += 1
+        sarga_number += 1
+        print(sarga_number)
+
+get_url()
